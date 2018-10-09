598c885fbe6f7da2ecfcc2175ff72027b056ccaf
Jeff E <jeffreyestany.gb@gmail.com>
2018-10-09 22:11:44 +0000

    refactored character counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,14 +1,16 @@
 $(document).ready(function() {
   // --- our code goes here ---
   $('#tweetBox').keyup( function() {
-    if ($(this).val().length > 140) {
-      if (!$(this).hasClass('negative')) {
-        $(this).siblings('.counter').addClass('negative')}
-      } else {
-        if (!$(this).hasClass('negative')) {
-        $(this).siblings('.counter').removeClass('negative')}
+    let $container = $(this).siblings('.counter');
+    let $tweetLength = $(this).val().length;
+    let $exceeds = $tweetLength > 140
+    let $hasclass = $container.hasClass('negative')
+    if ($exceeds && !$hasclass) {
+        $container.addClass('negative');
+      } else if (!$exceeds && $hasclass) {
+        $container.removeClass('negative');
       }
-    $(this).siblings('.counter').text(Number(140 - $(this).val().length))
+    $container.text(Number(140 - $tweetLength));
 
   })
 });
