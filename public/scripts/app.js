60c5e0bfb1a2fbfb40c77f9768f131d4a025b828
Jeff E <jeffreyestany.gb@gmail.com>
2018-10-11 17:16:06 +0000

    refactoring code

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,79 +3,79 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-$( document ).ready(function() {
 
-  $(function() {
-    var $button = $('#tweetForm');
-    $button.submit(function (event) {
-      event.preventDefault();
-      if ($('#tweetBox').val() == "") {
-        $("#tweetError").slideDown().text("ERROR:You may not tweet an empty tweet!");
-      }else if ($('#tweetBox').val().length < 141) {
-      var $serialized = $( tweetForm).serialize();
-      $.post("/tweets", $serialized)
-      .then(function() {
-        loadTweets();
-        $('#tweetBox').val("");
-        $("#tweetError").slideUp().text("");
-      }
-        );
-      } else {
-        $("#tweetError").slideDown().text("ERROR: You have exceeded the character limit for tweets!");
-      }
-    });
-  });
+$(document).ready(function() {
+  var $composeButton = $('#composeButton');
+  var $newTweet = $('.new-tweet');
+  var $tweetBox = $('#tweetBox');
+  var $tweetError = $("#tweetError");
+  var $tweetsContainer = $('#tweets-container');
+  var $button = $('#tweetForm');
 
-    $(function() {
-    var $composeButton = $('#composeButton');
-    $composeButton.click(function (event) {
-      if($('.new-tweet').is(":hidden")) {
-        $('.new-tweet').slideToggle()
-        $('#tweetBox').focus()
-      } else {
-        $('.new-tweet').slideToggle()
+  $button.submit(function (event) {
+    event.preventDefault();
+    if ($tweetBox.val() === "") {
+      $tweetError.slideDown().text("ERROR:You may not tweet an empty tweet!");
+    }else if ($tweetBox.val().length < 141) {
+    var $serialized = $tweetBox.serialize();
+    $.post("/tweets", $serialized)
+    .then(function() {
+      loadTweets();
+      $tweetBox.val("");
+      $tweetError.slideUp().text("");
     }
+      );
+    } else {
+      $tweetError.slideDown().text("ERROR: You have exceeded the character limit for tweets!");
+    }
+  });
+
+  function escape(str) {
+    let div = document.createElement('div');
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
+
+  function createTweetElement (tweetInfo) {
+    let tweetStamp = (new Date().getTime()) - (new Date(tweetInfo.created_at))
+   var tweet = `<article class="tweet">
+      <header>
+        <img src= ${tweetInfo.user.avatars.small} >
+        <span><h2>${tweetInfo.user.name}</h2> <h4>${tweetInfo.user.handle}</h4><span>
+      </header>
+      <section class="tweetbody">
+      ${escape(tweetInfo.content.text)}
+    </section>
+      <footer>
+        ${tweetStamp.toString()}ms ago(FIX!)
+      </footer>
+    </article>`;
+    return tweet
+  //DATESTAMP NEEDS TO BE FORMATTED (maybe use npm moment.js or time-ago ??)
+  };
+
+  function renderTweets(data) {
+    data.sort(function(a,b) {
+      return b.created_at - a.created_at;
     });
+    data.forEach(tweet => $tweetsContainer.append(createTweetElement(tweet)))
+  };
+
+  function loadTweets() {
+    $.getJSON("/tweets/", function (data) {
+      ($tweetsContainer.empty())
+      renderTweets(data)
+    })
+  };
+
+  $composeButton.click(function (event) {
+    if ($newTweet.is(":hidden")) {
+      $newTweet.slideToggle(function() {$tweetBox.focus()})
+    } else {
+      $newTweet.slideToggle()
+    }
   });
 
-function escape(str) {
-  let div = document.createElement('div');
-  div.appendChild(document.createTextNode(str));
-  return div.innerHTML;
-}
 
-
-function createTweetElement (tweetInfo) {
-  let tweetStamp = (new Date().getTime()) - (new Date(tweetInfo.created_at))
- var tweet = `<article class="tweet">
-    <header>
-      <img src= ${tweetInfo.user.avatars.small} >
-      <span><h2>${tweetInfo.user.name}</h2> <h4>${tweetInfo.user.handle}</h4><span>
-    </header>
-    <section class="tweetbody">
-    ${escape(tweetInfo.content.text)}
-
-  </section>
-    <footer>
-      ${tweetStamp.toString()}ms ago(FIX!)
-    </footer>
-  </article>`;
-  return tweet
-//DATESTAMP NEEDS TO BE FORMATTED (maybe use npm moment.js or time-ago ??)
-}
-
-function renderTweets(data) {
-  data.sort(function(a,b) {
-    return b.created_at - a.created_at;
-  });
-  data.forEach(tweet => $('#tweets-container').append(createTweetElement(tweet)))
-}
-
-function loadTweets() {
-  $.getJSON("/tweets/", function (data) {
-  ($('#tweets-container').empty())
-    renderTweets(data)})
-}
-
-loadTweets()
+  loadTweets();
 });
